e63df42781536c1671767f68ad886547c5283477
fedelaborda <101400350+fedelaborda@users.noreply.github.com>
2022-04-07 15:27:10 -0300

    Initial commit

---
+++ Assets/EJ1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//1.Crea un programa que permita ingresar por Inspector 
+//dos valores enteros y responda si son iguales o no lo 
+//son.
+
+public class EJ1 : MonoBehaviour
+{
+    public int numero1;
+    public int numero2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (numero1 == numero2)
+        {
+            Debug.Log("Estos numeros son iguales");
+        }
+
+        else
+        {
+            Debug.Log("Estos números no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
